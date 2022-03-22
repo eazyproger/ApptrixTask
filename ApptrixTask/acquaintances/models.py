54aa48e2751672ad3e3575b5a3715ede0be2bb0a
eazyproger <99medvedev@gmail.com>
2022-03-22 17:48:38 +0700

    Initial commit Create user model and its manager

---
+++ ApptrixTask/acquaintances/models.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import AbstractBaseUser
+from django.db import models
+
+from .manager import UserManager
+
+
+class CustomUser(AbstractBaseUser):
+    email = models.EmailField('email address', unique=True)
+    first_name = models.CharField(max_length=20)
+    second_name = models.CharField(max_length=30)
+    avatar = models.ImageField(upload_to='avatars')
+    gender = models.CharField(max_length=7)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
